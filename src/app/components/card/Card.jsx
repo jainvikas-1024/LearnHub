dbe8d1d6ddccc812bb1b6bd2b687e51de4071252
ParikshitGehlaut <139605784+ParikshitGehlaut@users.noreply.github.com>
2024-02-04 04:37:54 +0530

    Changed card component UI

--- src/app/components/card/Card.jsx
+++ src/app/components/card/Card.jsx
@@ -3,12 +3,13 @@ import Image from "next/image";
 import Link from "next/link";
 import styles from "./card.module.css";
 const Card = ({ post }) => {
+  console.log(post);
   return (
     <>
       <div className={styles.container}>
         <div className={styles.imageContainer}>
           <Image
-            src="/th.jpg"
+            src={post.img}
             alt="post image"
             height={300}
             width={320}
